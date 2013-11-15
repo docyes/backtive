e39f47e9e2696c431c0795f63b974bd079536a99
docyes <carl@featureblend.com>
2013-11-14 22:29:32 -0800

    stub for tests

---
+++ test/index.js
@@ -0,0 +1,11 @@
+(function() {
+    module('Backtive', {
+        setup: function() {
+        },
+        teardown: function() {
+        }
+    });
+    test('initialize', 0, function() {
+    
+    });
+})();
